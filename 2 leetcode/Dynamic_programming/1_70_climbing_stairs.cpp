f75b2289b4933132f7078fd947da41031a1ad998
Rituraj120705 <riturajkumar8804@gmail.com>
2025-08-18 05:40:06 +0530

    again this question by Tabulation  to reduce the time complexity

--- 2 leetcode/Dynamic_programming/1_70_climbing_stairs.cpp
+++ 2 leetcode/Dynamic_programming/1_70_climbing_stairs.cpp
@@ -80,5 +80,18 @@ public:
 
 
 
-// again this question is solved using the tabulation to reduce the time complexity
+// again this question is solved using the tabulation to reduce the time complexity it is the third method
 
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+
+    for(int i=2; i<=n; i++){
+        dp[i]=dp[i-1] + dp[i-2];
+    }    
+        return dp[n];
+    }
+};
